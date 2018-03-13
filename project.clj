4bfa97dcaecf2535756760c987826544f801ef50
Peter Marklund <peter@marklunds.com>
2018-03-13 12:26:34 +0100

    Upgraded to versioned 0.12.0 with version_token/preview support

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject versioned-example "0.1.0-SNAPSHOT"
   :description "Clojure CMS REST API based on MongoDB for www.marklunds.com"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [versioned "0.11.26"]]
+                 [versioned "0.12.0"]]
   :min-lein-version "2.0.0"
   :uberjar-name "marklunds-standalone.jar"
   :main ^:skip-aot app.core
